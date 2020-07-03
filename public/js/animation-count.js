158cf4dbbc897989f6d7d8804f5587f93aa53ce9
Nuriddin Muhammadjanov <nmuhammadjanov@gmail.com>
2020-07-03 13:49:00 +0500

    qwerty

--- public/js/animation-count.js
+++ public/js/animation-count.js
@@ -1,33 +1,33 @@
-let counted = false
+let counted = false;
 
 $('.section2').waypoint(function (direction) {
     $('.section2').addClass("fade-in")
 }, {
-    offset: '80%'
+    offset: '40%'
 });
 
 $('.section3').waypoint(function (direction) {
     $('.section3').addClass("fade-in")
 }, {
-    offset: '80%'
+    offset: '40%'
 });
 
 $('.section4').waypoint(function (direction) {
     $('.section4').addClass("fade-in")
 }, {
-    offset: '80%'
+    offset: '40%'
 });
 
 $('.section5').waypoint(function (direction) {
     $('.section5').addClass("fade-in")
 }, {
-    offset: '80%'
+    offset: '40%'
 });
 
 $('.section6').waypoint(function (direction) {
     $('.section6').addClass("fade-in")
 }, {
-    offset: '80%'
+    offset: '40%'
 });
 
 $('.section7').waypoint(function (direction) {
@@ -65,37 +65,37 @@ $('.section7').waypoint(function (direction) {
 $('.section8').waypoint(function (direction) {
     $('.section8').addClass("fade-in")
 }, {
-    offset: '80%'
+    offset: '40%'
 });
 
 $('.section9').waypoint(function (direction) {
     $('.section9').addClass("fade-in")
 }, {
-    offset: '80%'
+    offset: '40%'
 });
 
 $('.section10').waypoint(function (direction) {
     $('.section10').addClass("fade-in")
 }, {
-    offset: '80%'
+    offset: '40%'
 });
 
 $('.section11').waypoint(function (direction) {
     $('.section11').addClass("fade-in")
 }, {
-    offset: '80%'
+    offset: '40%'
 });
 
 $('.section12').waypoint(function (direction) {
     $('.section12').addClass("fade-in")
 }, {
-    offset: '80%'
+    offset: '40%'
 });
 
 $('.section13').waypoint(function (direction) {
     $('.section13').addClass("fade-in")
 }, {
-    offset: '80%'
+    offset: '40%'
 });
 
 $('.section14').waypoint(function (direction) {
